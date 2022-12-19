93e4978421617a9d18de03608e4d18fdd2547ac4
NaOHsolution <2974910797@qq.com>
2022-12-19 08:45:54 +0800

    1.1.0 Update
    1. Endless input
    2. Input verification

--- Factorizer/main.cpp
+++ Factorizer/main.cpp
@@ -1,5 +1,7 @@
 #include <conio.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 #include <vector>
 
 using namespace std;
@@ -10,30 +12,50 @@ ull number;
 vector<pair<ull, short>> result;
 
 int main() {
-	cout << "Enter the number to be factorized: ";
-	cin >> number;
-
-	ull t = number;
+	ull t;
 	short cnt;
-	for (ull i = 2; i <= t; ++i) {
-		if (t % i == 0) {
-			cnt = 0;
-			while (t % i == 0) t /= i, ++cnt;
-			result.push_back(pair<ull, short>(i, cnt));
+	string buf;
+	stringstream m;
+
+	while (true) {
+		m.clear();
+		m.str("");
+
+		cout << "Enter the number to be factorized (0 to exit): ";
+		getline(cin, buf, '\n');
+		m << buf;
+		m >> number;
+		if (m.fail()) {
+			cout << endl << "Invalid input." << endl << endl;
+			continue;
 		}
-	}
 
-	if (number != 1) cout << endl << number << " = ";
-	for (vector<pair<ull, short>>::iterator itr = result.begin(); itr != result.end(); ++itr) {
-		if (itr->second == 1) cout << itr->first;
-		else cout << itr->first << " ^ " << itr->second;
-		if (itr + 1 != result.end()) cout << " x ";
-	}
-	if (number == 1) cout << endl << "1 is neither prime nor composite.";
-	else if (result.size() == 1) cout << endl << number << " is prime.";
-	else cout << endl << number << " is composite.";
+		if (number == 0) break;
 
-	cout << endl << endl << "Press any key to exit...";
-	while (!_kbhit());
+		t = number;
+		result.clear();
+		if (number != 1) {
+			for (ull i = 2; i <= t; ++i) {
+				if (t % i == 0) {
+					cnt = 0;
+					while (t % i == 0) t /= i, ++cnt;
+					result.push_back(pair<ull, short>(i, cnt));
+				}
+			}
+
+			cout << endl << number << " = ";
+			for (vector<pair<ull, short>>::iterator itr = result.begin(); itr != result.end(); ++itr) {
+				if (itr->second == 1) cout << itr->first;
+				else cout << itr->first << " ^ " << itr->second;
+				if (itr + 1 != result.end()) cout << " x ";
+			}
+			cout << endl;
+			if (result.size() == 1) cout << number << " is prime.";
+			else cout << number << " is composite.";
+		}
+		else cout << endl << "1 is neither prime nor composite.";
+		cout << endl << endl;
+	}
+	
 	return 0;
 }
